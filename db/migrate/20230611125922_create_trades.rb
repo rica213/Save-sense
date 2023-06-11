0b30f89302483371c05e3567c0173ae02fb96e38
rica213 <25f5k252l@mozmail.com>
2023-06-11 16:31:02 +0300

    Create Schema using migration

---
+++ db/migrate/20230611125922_create_trades.rb
@@ -0,0 +1,11 @@
+class CreateTrades < ActiveRecord::Migration[7.0]
+  def change
+    create_table :trades do |t|
+      t.string :name
+      t.decimal :amount
+      t.references :author, foreign_key: { to_table: :users }
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,54 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_06_11_130108) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "trade_categories", force: :cascade do |t|
+    t.bigint "trade_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_trade_categories_on_category_id"
+    t.index ["trade_id"], name: "index_trade_categories_on_trade_id"
+  end
+
+  create_table "trades", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_trades_on_author_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "categories", "users"
+  add_foreign_key "trade_categories", "categories"
+  add_foreign_key "trade_categories", "trades"
+  add_foreign_key "trades", "users", column: "author_id"
+end

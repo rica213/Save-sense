b1e10fca072996290c4378113278f368aa409c40
rica213 <25f5k252l@mozmail.com>
2023-06-12 07:48:34 +0300

    Fixbug: Fix association setup to allow creating category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,14 +2,14 @@ class Category < ApplicationRecord
   # Associations
   # A category belongs to a user
   # A category can have one or many trades
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :trade_categories, dependent: :destroy
   has_many :trades, through: :trade_categories
 
   # Validations
   # A category must have a name
-  validates :name, presence: true
+  # validates :name, presence: true
 
   # A category name must not exceed 50 characters
-  validates :name, length: { maximum: 50 }
+  # validates :name, length: { maximum: 50 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
   # Associations
   # A user can make one or many trades
   # A user can create one or many categories
-  has_many :trades, dependent: :destroy
-  has_many :categories, dependent: :destroy
+  has_many :trades, foreign_key: 'author_id', dependent: :destroy
+  has_many :categories, foreign_key: 'author_id', dependent: :destroy
 
   # Validations
   # A user must have a name
-  validates :name, presence: true
+  # validates :name, presence: true
 
   # A user name must not exceed 50 characters
-  validates :name, length: { maximum: 50 }
+  # validates :name, length: { maximum: 50 }
 end

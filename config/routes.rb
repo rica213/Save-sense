bb2efa97fa9ab8b386901ac1689a4e52c0af59cf
rica213 <25f5k252l@mozmail.com>
2023-06-12 12:46:27 +0300

    Set category index page as homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :categories, only: [:index, :new] 
+  
+  root "categories#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end

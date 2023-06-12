b22abbc6bda190ec90ac69cbd28ed4d72109a8d0
rica213 <25f5k252l@mozmail.com>
2023-06-12 18:08:55 +0300

    Implement the splash action for the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :categories, only: [:index, :new] 
-  
-  root "categories#index"
 
+  root "users#splash"
+
+  resources :categories, only: [:index, :new]
+  get '/splash', to: 'users#splash'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end

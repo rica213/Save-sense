520899146d562530cf1fd7baadfb5a78662605f7
rica213 <25f5k252l@mozmail.com>
2023-06-12 18:12:10 +0300

    Refactor: Validate redirection

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Categories', type: :request do
   describe 'GET categories/' do
-    it 'returns http success' do
+    it 'returns http redirect' do
       get '/categories/'
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:redirect)
     end
   end
 
   describe 'GET /new' do
-    it 'returns http success' do
+    it 'returns http redirect' do
       get '/categories/new'
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:redirect)
     end
   end
 end

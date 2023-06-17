61f2a2a946fb0381b04816ab8fcd36792d524069
Tao (⌊arissa) <25f5k252l@mozmail.com>
2023-06-17 21:59:20 +0300

    Remove show category from route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root "users#splash"
 
   resources :trades, only: [:index, :show, :new, :create]
-  resources :categories, only: [:index, :new, :create, :show] do
+  resources :categories, only: [:index, :new, :create] do
     resources :trades, only: [:index]
   end
   get "/splash", to: "users#splash"
